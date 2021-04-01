13fe1f7634cb1d358e0d0757662785f28704e941
thecodeplanner <thecodeplanner@gmail.com>
2021-04-01 13:51:56 -0400

    added comments

--- algo-practice.js
+++ algo-practice.js
@@ -3,7 +3,7 @@
 // Given two strings write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 
 function validAnagram(string1, string2){
-    if (string1.length !== string2.length) {
+    if (string1.length !== string2.length) {  // if strings are not same length, this should return false 
         return false
     }
     let checkLetters = {}
@@ -13,6 +13,7 @@ function validAnagram(string1, string2){
     }
     for (let i = 0; i < string2.length; i++) {
         let letters = string2[i]
+        // if can't find letter or letter is 0, then not an anagram
         if (!checkLetters[letters]) {
             return false
         }else {
