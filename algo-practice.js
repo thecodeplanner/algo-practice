bf95f678f257e470108d9282f269f9b29434a1e8
thecodeplanner <thecodeplanner@gmail.com>
2021-04-01 16:14:02 -0400

    added solutions

--- algo-practice.js
+++ algo-practice.js
@@ -113,3 +113,40 @@ function maxSubarraySum(array, num) {
     }
     return maxSum;
 }
+
+
+
+// Divide and Conquer - Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1.
+// search([1,2,3,4,5,6], 4) // 3 
+
+function search(array, value) {
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i;
+        }else {
+            return -1
+        }
+    }
+}
+
+// another solution would be binary search
+
+function search(array, value) {
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2)
+        let currentElement = array[middle];
+
+        if (array[middle] < value) {
+            min = middle + 1;
+        }else if (array[middle] > value) {
+            max = middle - 1;
+        }else {
+            return middle
+        }
+    }
+    return -1
+}
+
