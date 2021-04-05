a02915e3b1948d1ccbb52f37389568088ecc14f3
thecodeplanner <thecodeplanner@gmail.com>
2021-04-04 23:25:23 -0400

    added algo

--- algo-practice.js
+++ algo-practice.js
@@ -213,3 +213,28 @@ function isPalindrome(string){
       }
       return true 
   }
+
+
+// capitalizeFirst 
+// Write a recursive function called capitalizeFirst. Given an array of strings, apitalize the first letter of each string in the array. 
+// capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+
+function capitalizeFirst (array) {
+    if (array.length === 1) {
+      return [array[0][0].toUpperCase() + array[0].substr(1)]; // grab first word in array and then first character and capitalize
+    }
+    const res = capitalizeFirst(array.slice(0, -1));
+    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+    res.push(string);
+    return res;
+  }
+
+// function capitalizeWords (array) {
+//     if (array.length === 1) {
+//       return [array[0].toUpperCase()];
+//     }
+//     let res = capitalizeWords(array.slice(0, -1));
+//     res.push(array.slice(array.length-1)[0].toUpperCase());
+//     return res;
+   
+//   }  
