76fac482b443200400a8553d80ef5c341b6a55f7
thecodeplanner <thecodeplanner@gmail.com>
2021-04-06 17:11:59 -0400

    updates

--- bubbleSort.js
+++ bubbleSort.js
@@ -5,11 +5,15 @@ function bubbleSort(array) {
         [array[index1], array[index2]] = [array[index2], array[index1]]
     }
     for (let i = array.length; i > 0; i--) { // had i start at end of array, while i is greater than 0, i --
+        let noSwaps = true 
         for (let j = 0; j < i - 1; j++) { //this inner loop then will just compare the 2 numbers, but as we increment the index, we don't have to compare the last index anymore as it has been sorted 
+            console.log(array, array[j], array[j+1])
             if (array[j] > array[j+1]) {
                 swap(array, j, j+1)
+                noSwaps = false
             }
         }
+        if (noSwaps) break // this is so that if there are no more comparisons, we can break out of the loop early 
 
     }
     return array
