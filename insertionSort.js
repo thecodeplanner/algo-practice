de032219f9eef591eeff9c6045679f83d3984409
thecodeplanner <thecodeplanner@gmail.com>
2021-04-13 18:08:14 -0400

    solution

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let currentVal = array[i];
+        for (let j = i-1; j >= 0 && array[j] > currentVal; j--) {
+            array[j+1] = array[j]
+        }
+        array[j+1] = currentVal
+    }
+    return array
+}
+
+insertionSort([2,1,9.35,6])
