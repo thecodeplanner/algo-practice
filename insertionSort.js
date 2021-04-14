1b5f0bffe81744b0c6b3619ef9cf5a0f83ccb6d3
thecodeplanner <thecodeplanner@gmail.com>
2021-04-14 12:34:46 -0400

    updated solution and comments

--- insertionSort.js
+++ insertionSort.js
@@ -1,13 +1,13 @@
-
 function insertionSort(array) {
-    for (let i = 1; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) { // have i start at 1 and 2nd loop will start and compare always 1 minus i
         let currentVal = array[i];
-        for (let j = i-1; j >= 0 && array[j] > currentVal; j--) {
-            array[j+1] = array[j]
-        }
-        array[j+1] = currentVal
+        let j = i-1
+        while (j >= 0 && array[j] > currentVal) { //while value of j is still greater than current value, continue loop
+            array[j+1] = array[j] // make and move array[j] to be in index ahead
+            j-- // move index down to check other values 
+        } array[j+1] = currentVal // update current value so that it will be in index of j's original spot 
     }
     return array
 }
-
-insertionSort([2,1,9.35,6])
+                  
+insertionSort([1,2,9,35,6])
