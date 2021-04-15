eabc61c8a436842ea2f9770c74f2261e19165086
thecodeplanner <thecodeplanner@gmail.com>
2021-04-15 11:46:11 -0400

    added notes

--- mergeSort.js
+++ mergeSort.js
@@ -1,6 +1,6 @@
 // create a function that takes in 2 unsorted arrays and returns 1 sorted array 
 
-function mergeSort(arr1, arr2) {
+function merge(arr1, arr2) {
     let sorted = []
     let i = 0;
     let j = 0;
@@ -24,3 +24,16 @@ function mergeSort(arr1, arr2) {
    }
    return sorted
 }
+
+// writing a function recursively 
+// time complexity for this is o(n log n)
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr; // need to always have base case 
+    let mid = Math.floor(arr.length/2) // gives us halfway point
+    let left = mergeSort(arr.slice(0, mid)); //call mergeSort on left half of array until we reach length of 1. from there we have our left array element
+    let right = mergeSort(arr.slice(mid)(); // call mergeSort on right half of array until we reach length of less than or equal to 1
+    return merge(left, right); // call merge on the left and right numbers and sort them, do this with all numbers in array
+}
+
+// mergeSort([10,24,76,73,21,1,9])
