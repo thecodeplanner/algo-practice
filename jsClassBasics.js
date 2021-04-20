d29b1a78985346f18b5cf9ad3b7fa61c8f6e5408
thecodeplanner <thecodeplanner@gmail.com>
2021-04-20 13:16:03 -0400

    examples

--- jsClassBasics.js
+++ jsClassBasics.js
@@ -6,13 +6,19 @@ class Student {
         this.lastName = lastName;
         this.grade = year; // notice that we can name the property whatever we'd like
     }
+    fullName() {
+        return  `Your full name is ${this.firstName} ${this.lastName}.` //this allows us to create instance methods
+    }
 }
 
+// this refers to the instance of the class**
+
 // above is just a blueprint of the student class 
-
 // we need to instatiate in order to create new students below - 
+let firstStudent = new Student("Melissa", "Mach", 5)
+let secondStudent = new Student("Jo", "Malone", 2)
 
-let firstStudent = new Student("Melissa", "Mach")
-let secondStudent = new Student("Jo", "Malone")
+// now that students are created, we can call on their properties which helps to organize our code 
+// we are also able to call on the instance method -- make sure to invoke the function otherwise we will just receive the function definiteion
 
-// now that students are created, we can call on their properties which helps to organize our code
+firstStudent.fullName() // Your full name is Melissa Mach.
