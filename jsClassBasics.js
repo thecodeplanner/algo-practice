ecb3d1a71c0dd07ecb663b4cc9b5e02ed885b24c
thecodeplanner <thecodeplanner@gmail.com>
2021-04-20 13:49:08 -0400

    added additional methods and comments

--- jsClassBasics.js
+++ jsClassBasics.js
@@ -5,9 +5,26 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.grade = year; // notice that we can name the property whatever we'd like
+        this.tardies = 0;
+        this.scores = [];
     }
     fullName() {
         return  `Your full name is ${this.firstName} ${this.lastName}.` //this allows us to create instance methods
+    }
+    markLate() {
+        this.tardies += 1;
+        if(this.tardies >= 3) {
+            return 'You are suspended!'
+        }
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times(s).`
+    }
+    addScore(score) { //we can also pass in an argument with an instance method 
+        this.scores.push(score); //there is nothing stopping us from pushing right to score property, but this is better convention
+        return this.scores
+    }
+    calculateAverage() {
+        let sum = this.scores.reduce(function(a,b) {return a+b})
+        return sum/this.scores.length
     }
 }
 
