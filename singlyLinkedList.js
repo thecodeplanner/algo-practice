f0e7f4bc0c1c73831e8db267754252f2659d8e59
thecodeplanner <thecodeplanner@gmail.com>
2021-04-22 01:13:13 -0400

    add pop method

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -23,7 +23,24 @@ class SinglyLinkedList{
         this.length++; // need to increment the length of the list
         return this; // return the list at the end 
 
-    })
+    }
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) { //while there is still a node after, make the newTail equal to current and current becomes the next node
+            newTail = current;
+            current = current.next
+        }
+        this.tail = newTail; //then we make newTail the tail
+        this.tail.next = null; //the node afterwards becomes null
+        this.length--; //we decrement the length of the list because we are removing a node
+        if (this.length === 0) { //once there are no more nodes, we set the head and tail to null 
+            this.head = null;
+            this.tail = null;
+        }
+        return current; //return the last node that was popped off 
+    }
 }
 
 let list = new SinglyLinkedList() //created new instance and set it to list
